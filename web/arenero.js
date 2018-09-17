baace837e88090adc48a25a88399ea983c516a5c
Emilio <emilioplatzer@gmail.com>
2018-09-17 14:25:31 -0300

    muestra el error de compilaciÃn (pero no en quÃ linea)

--- web/arenero.js
+++ web/arenero.js
@@ -21,38 +21,48 @@ var sandbox={
 function probar(){
     var texto = funcion.value;
     sandbox.parametros=[];
-    sandbox.funcion = new Function('args','var f='+texto+';\n return f.apply(null,args);');
-    var info = parsear(texto);
     parametros.innerHTML="";
-    agregarLinea(parametros,'función '+info[1]);
-    var nombres_parametros=info[2].split(',');
-    var tabla=document.createElement('table');
-    nombres_parametros.forEach(function(nombre,i){
-        var fila = tabla.insertRow(-1);
-        var celda_nombre = fila.insertCell(-1);
-        celda_nombre.textContent = nombre;
-        var input = document.createElement('input');
-        var celda_input = fila.insertCell(-1);
-        celda_input.appendChild(input);
-        sandbox.parametros.push(null);
-        input.onblur=function(){
-            sandbox.parametros[i] = input.value;
+    try{
+        sandbox.funcion = null;
+        sandbox.funcion = new Function('args','var f='+texto+';\n return f.apply(null,args);');
+    }catch(err){
+        agregarLinea(parametros,'error en la escritura de la función');
+        agregarLinea(parametros,err.message);
+        console.log(err)
+        agregarLinea(parametros,err);
+    }
+    if(sandbox.funcion){
+        var info = parsear(texto);
+        agregarLinea(parametros,'función '+info[1]);
+        var nombres_parametros=info[2].split(',');
+        var tabla=document.createElement('table');
+        nombres_parametros.forEach(function(nombre,i){
+            var fila = tabla.insertRow(-1);
+            var celda_nombre = fila.insertCell(-1);
+            celda_nombre.textContent = nombre;
+            var input = document.createElement('input');
+            var celda_input = fila.insertCell(-1);
+            celda_input.appendChild(input);
+            sandbox.parametros.push(null);
+            input.onblur=function(){
+                sandbox.parametros[i] = input.value;
+            };
+        });
+        var boton=document.createElement('button');
+        var fila=tabla.insertRow(-1);
+        fila.insertCell(-1);
+        var celda_boton = fila.insertCell(-1);
+        celda_boton.appendChild(boton);
+        boton.textContent='correr';
+        fila=tabla.insertRow(-1);
+        fila.insertCell(-1);
+        var celda_resultado = fila.insertCell(-1);
+        boton.onclick=function(){
+            var resultado = sandbox.funcion.call(this, sandbox.parametros);
+            celda_resultado.textContent=resultado;
         };
-    });
-    var boton=document.createElement('button');
-    var fila=tabla.insertRow(-1);
-    fila.insertCell(-1);
-    var celda_boton = fila.insertCell(-1);
-    celda_boton.appendChild(boton);
-    boton.textContent='correr';
-    fila=tabla.insertRow(-1);
-    fila.insertCell(-1);
-    var celda_resultado = fila.insertCell(-1);
-    boton.onclick=function(){
-        var resultado = sandbox.funcion.call(this, sandbox.parametros);
-        celda_resultado.textContent=resultado;
-    };
-    parametros.appendChild(tabla);
+        parametros.appendChild(tabla);
+    }
 }
 
 window.addEventListener('load',function(){
